344982eb706efb73798cb58131286f3c24e0f0b0
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-06-12 00:52:01 -0300

    Clase 9-a Registros con Postgresql y Python TERMINADA

---
+++ AndresAbdalaAlvarez/PYTHO2024/Leccion18/BD/delete_varios_registros.py
@@ -0,0 +1,24 @@
+import psycopg2 #Esto es para conectarnos a PostgreSQL
+
+conexion = psycopg2.connect(
+    user = 'postgres',
+    password = '34016357',
+    host = '127.0.0.1',
+    port ='5432',
+    database ='test_bd'
+)
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            entrada = input("Digite los numeros de registro a eliminar(separados por coma): ")
+            valores = (tuple(entrada.split(',')),)  # Es una tupla de tuplas
+            cursor.execute(sentencia, valores) #De esta manera ejecutamos la sentencia
+            registros_eliminados = cursor.rowcount #Recuperamos todos los registros que seran una lista
+            print(f"Los registros eliminados son: {registros_eliminados}")
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
