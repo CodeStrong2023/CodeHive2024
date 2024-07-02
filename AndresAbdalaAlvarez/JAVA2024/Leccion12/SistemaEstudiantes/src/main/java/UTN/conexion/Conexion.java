2c7059af7242bf03c484676353d1fba5f8043299
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-07-02 01:30:08 -0300

    Terminando Clase 12 Proyecto estudiantes Parte 2

--- AndresAbdalaAlvarez/JAVA2024/Leccion12/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
+++ AndresAbdalaAlvarez/JAVA2024/Leccion12/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     public static Connection getConnection(){
         Connection conexion = null;
         //Variables para conectarnos a la base de datos
-        var baseDatos = "estudiantes2022";
+        var baseDatos = "estudiantes";
         var url = "jdbc:mysql://localhost:3306/" + baseDatos;
         var usuario = "root";
         var password = "admin";
@@ -16,7 +16,7 @@ public class Conexion {
 
         //cargamos la clase del driver de mySQL en memoria
         try{
-            conexion = DriverManager.getConnection(url, usuario, password)
+            conexion = DriverManager.getConnection(url, usuario, password);
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch(ClassNotFoundException | SQLException e){
             System.out.println("Ocurrio un error en la conexion: "+ e);
